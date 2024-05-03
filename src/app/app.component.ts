1ae5e2b0649b28f33234866c21254b4780046132
Claudio <claudio.riquelme@consultores.wom.cl>
2024-05-03 13:02:57 -0400

    Calculadora con Angular V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'calculadora-app';
+  titulo = 'Aplicación Calculadora';
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+
+  sumar(): void {
+    this.resultado = this.operandoA + this.operandoB;
+  }
+
+  restar(): void {
+    this.resultado = this.operandoA - this.operandoB;
+  }
+
+  multiplicar(): void {
+    this.resultado = this.operandoA * this.operandoB;
+  }
+
+  dividir(): void {
+    this.resultado = this.operandoA / this.operandoB;
+  }
 }
